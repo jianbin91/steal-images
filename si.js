dc879733f399d7096a2303867f2d01461a0d353f
Gabor Darvas <nagyfej@gmail.com>
2013-09-02 08:46:38 +0200

    working with links only

--- si.js
+++ si.js
@@ -1,48 +1,98 @@
 var fs = require('fs');
-var http = request('http');
-
-var imageDir = './images';
+var http = require('http');
+var htmlparser = require('htmlparser2');
+var path = require('path');
+var async = require('async');
+var request = require('request');
 
 var source = process.argv[2];
 
+var tagName = process.argv[3] == '--img' ? 'img' : 'a';
+console.log('Mode: ' + tagName);
+var timestamp = Math.round(new Date().getTime() / 1000);
+
+var imageDir = './images';
+var destinationDir = imageDir + '/' + timestamp;
+
 var imageList=[];
+
+var limit = 30;
 
 console.log("Welcome to steal-images.js.");
 
-//init
+//create IMG dir if doesn't exists
 fs.exists(imageDir, function (exists) {
   if(exists){ 
   	console.log("Image dir already exists.");
   }
   else{
-  	fs.mkdir(imagedir, function(){
+  	fs.mkdir(imageDir, function(){
   		console.log('Image dir created.');
   	});
   }
-  
 });
 
-//create IMG dir if doesn't exists
 
-//create dest dir if doesn't exists
+var createDestinationDir = function(callback){
+  	fs.mkdir(destinationDir, function(){
+  		console.log('Destination dir created.');
+  		callback();
+  	});
+};
 
-//request URL
-http.get(source, function(res) {
-  //get content to parser
-	res.on('data', function(data){
-		
+//get parser ready
+var parser = new htmlparser.Parser({
+    onopentag: function(name, attribs){
+        if(name === tagName){
+        	var name = tagName == 'a' ? attribs.href : attribs.src;
+        	console.log(name);
+        	var url = source + name;
+        	var ext = path.extname(attribs.href).toLowerCase();
+            if(ext === '.jpg' || ext === '.jpeg' || ext == '.gif' || ext == '.png'){
+            	var fn = function(){
+            		request.head(url, function(err, res, body){
+            			console.log(name + ' downloaded.');
+					    request(url).pipe(fs.createWriteStream(destinationDir + '/' + name));
+					});
+            	};
+            	imageList.push(fn);
+            }  
+        }
+    }
+});
+
+var getHtml = function(){
+	//request URL
+	http.get(source, function(res) {
+		//get content to parser
+		res.on('data', function(chunk){
+			parser.write(chunk);
+		});
+		res.on('end', function(){
+			console.log('HTML parsed. ' + imageList.length + ' images found.');
+			parser.end();
+			if(imageList.length > 0)
+				createDestinationDir(executeSave);
+			else
+				console.log('Nothing to save.');
+		});
+			//get image sources to array
+	}).on('error', function(e) {
+	  console.log("Error fetching remote URL: " + e.message);
 	});
-		//get image sources to array
-}).on('error', function(e) {
-  console.log("Error fetching remote URL: " + e.message);
-});
+};
 
-	
-		
-			
-			
-//iterate array
+var executeSave = function(){
+	async.parallel(imageList, function(err, results){
+		if(err)
+		  console.log('Something went wrong: ' + err.message);
+		else
+		  console.log('Image donwloads are done.');
+	});
+};
 
-	//fetch image url as stream to dest dir
-
-//all done.
+//after INIT need to call getHtml
+if(!source)
+	console.log('Missing source URL');
+else
+	getHtml();
